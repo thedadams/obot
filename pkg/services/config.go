f8c1a7ff95499886fcd85e468908f2b6717f2df3
Donnie Adams <donnie@acorn.io>
2025-05-09 14:13:23 -0400

    Hide the kubernetes based arguments

--- pkg/services/config.go
+++ pkg/services/config.go
@@ -93,8 +93,8 @@ type Config struct {
 	StaticDir                  string   `usage:"The directory to serve static files from"`
 	RetentionPolicyHours       int      `usage:"The retention policy for the system. Set to 0 to disable retention." default:"2160"` // default 90 days
 	MCPCatalogs                []string `usage:"Load MCP catalogs, these can be files or URLs" split:"true"`
-	MCPBaseImage               string   `usage:"The base image to use for MCP containers"`
-	MCPClusterDomain           string   `usage:"The cluster domain to use for MCP containers" default:"cluster.local"`
+	MCPBaseImage               string   `usage:"The base image to use for MCP containers" hidden:"true"`
+	MCPClusterDomain           string   `usage:"The cluster domain to use for MCP containers" default:"cluster.local" hidden:"true"`
 	// Sendgrid webhook
 	SendgridWebhookUsername string `usage:"The username for the sendgrid webhook to authenticate with"`
 	SendgridWebhookPassword string `usage:"The password for the sendgrid webhook to authenticate with"`
