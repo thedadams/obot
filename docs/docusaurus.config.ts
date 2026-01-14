1d9fec7e706fbfbf6545c33fa8a050a9c0b1cba8
Craig Jellick <craig@obot.ai>
2026-01-14 11:25:18 -0700

    docs: fix one more broken referral link
    Signed-off-by: Craig Jellick <craig@obot.ai>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -38,6 +38,10 @@ const config: Config = {
             from: "/installation/general",
             to: "/installation/overview",
           },
+          {
+            from: "/tutorials/knowledge-assistant",
+            to: "/",
+          },
         ],
       },
     ],
